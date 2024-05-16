9a75a7615720c02c4f6497b66b2f530c53ba07f0
Lina-Bensenouci <119866419+Lina-Bensenouci@users.noreply.github.com>
2024-05-16 11:10:47 -0400

    ajustement des boutons précédent et suivant

--- carrousel.php
+++ carrousel.php
@@ -9,36 +9,40 @@
  */
 
  function enqueue_style_script(){
+ 
   $version_css = filemtime(plugin_dir_path( __FILE__ ) . "style.css");
   $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
-  
+ 
   wp_enqueue_style(   'em_plugin_carrousel_css',
-                    plugin_dir_url(__FILE__) . "style.css",
-                    array(),
-                    $version_css);
-
-    wp_enqueue_script(  'em_plugin_carrousel_js',
-                      plugin_dir_url(__FILE__) ."js/carrousel.js",
-                      array(),
-                      $version_js,
-                      true);
- }
-
- add_action('wp_enqueue_scripts','enqueue_style_script');
-
-function genere_html(){
-
-  $html = '
-    <button class="bouton__ouvrir">Ouvrir Carrousel</button>
-    <div class="carrousel">
-    <a href="" class="carrousel__x">x</a>
-    <figure class="carrousel__figure"></figure>
-    <form action="" class="carrousel__action"</form>
-    </div>
-  ';
-  
-
-  return $html;
+        plugin_dir_url(__FILE__) . "style.css",
+        array(),
+        $version_css);
+ 
+  wp_enqueue_script(  'em_plugin_carrousel_js',
+        plugin_dir_url(__FILE__) ."js/carrousel.js",
+        array(),
+        $version_js,
+        true);
+ 
+ 
 }
-
+add_action('wp_enqueue_scripts', 'enqueue_style_script');
+ 
+ 
+ 
+function genere_html(){
+   
+  $html = '
+  <button class="bouton__ouvrir">Ouvrir Carrousel</button>
+  <div class="carrousel">
+      <a href="" class="carrousel__x">X</a>
+      <figure class="carrousel__figure"></figure>
+      <form action="" class="carrousel__action"</form>
+      </div>
+ 
+   
+';
+return $html;
+}
+ 
 add_shortcode('carrousel', 'genere_html');
